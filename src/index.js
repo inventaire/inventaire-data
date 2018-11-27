35341386dd5a2defefc15bc33d496c2fb5782e43
maxlath <k@maxlath.eu>
2018-11-27 15:46:36 -0800

    index: adding social network metadata

--- src/index.js
+++ src/index.js
@@ -6,6 +6,19 @@ module.exports = data => {
     <meta charset="utf-8" />
     <title>Inventaire data</title>
     <link rel="stylesheet" href="style.css">
+    <!-- TWITTER -->
+    <meta name="twitter:title" content="Inventaire - The Wikidata-federated open bibliographic database">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@inventaire_dev">
+    <meta name="twitter:description" content="The Inventaire project has two cores working in synergy: this page is about the open data project">
+    <meta name="twitter:image" content="https://data.inventaire.io/assets/inv_data_screenshot.jpg">
+
+    <!-- FACEBOOK -->
+    <meta property="og:site_name" content="Inventaire Data">
+    <meta property="og:url" content="https://data.inventaire.io/">
+    <meta property="og:title" content="Inventaire - The Wikidata-federated open bibliographic database">
+    <meta name="description" property="og:description" content="The Inventaire project has two cores working in synergy: this page is about the open data project" />
+    <meta property="og:image" content="https://data.inventaire.io/assets/inv_data_screenshot.jpg">
   </head>
   <body>
     <section id="landing">
